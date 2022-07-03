6148c7494742106cbfb4e840c22f93f7014fbeed
Елена Аль Ма ая <96118677+ElenaMaaya@users.noreply.github.com>
2022-07-03 18:09:36 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from QAP_28.pages.locators import StartLocators
+from pages.locators import StartLocators
 
 
 class BasePage(object):
