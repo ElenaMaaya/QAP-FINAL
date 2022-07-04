35ff3dd90697983b7fb837c3c31600fe5664a0f1
Елена Аль Ма ая <96118677+ElenaMaaya@users.noreply.github.com>
2022-07-04 09:14:55 +0300

    Update base_page.py

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,10 +1,8 @@
 from time import sleep
 from urllib.parse import urlparse
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-from pages.locators import StartLocators
 
 
 class BasePage(object):
